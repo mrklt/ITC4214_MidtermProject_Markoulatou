ae9e0436e507d259cf9a629841af3317d7c8576f
mrklt <m.markoulatou@acg.edu>
2024-10-21 19:48:37 +0300

    tasks left: fix javascript, finish the report file, move js to the head

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,8 @@
-function SwitchMode() {
-    let element = document.body;
-    element.classList.toggle("dark");
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.querySelector('.toggle');
+
+    // Toggle dark mode when the button is clicked
+    toggleButton.addEventListener('click', function() {
+        document.body.classList.toggle("dark");
+    });
+});
