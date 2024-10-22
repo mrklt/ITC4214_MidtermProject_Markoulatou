e4bd447d0053fdb7f56b446ade5d6bf4fe7b6493
mrklt <m.markoulatou@acg.edu>
2024-10-22 23:06:38 +0300

    Report page

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.querySelector('.toggle');
 
-    // Toggle dark mode when the button is clicked
     toggleButton.addEventListener('click', function() {
         document.body.classList.toggle("dark");
     });
